d5c921ac67f7ec291d86d7ac0e86086fab59ee23
vadim shaxverdiev <you@example.com>
2024-10-24 21:40:44 +0300

    update structure

--- velora/velora_app/backend/service1/go.mod
+++
@@ -1,3 +0,0 @@
-module service1
-
-go 1.23.2
--- velora/velora_app/frontend/vue-frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './styles.css'; // Подключение стилей
-
-createApp(App).mount('#app');
